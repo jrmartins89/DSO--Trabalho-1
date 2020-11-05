a4f4af546c801b3ab64f0c7cd1096e1e679569aa
jrmartins89 <73797262+jrmartins89@users.noreply.github.com>
2020-11-05 00:12:23 -0300

    Quarto commit
    Ajuste no construtor das classes "Jogo", "Aquisicao", "Usuario";
    Ajuste nos setter das classses "Jogo", "Aquisicao", "Usuario" para inclusão de verificação do tipo da variável;

---
+++ Model/Aquisicao.py
@@ -0,0 +1,35 @@
+from MVC.model.usuario import Usuario
+from MVC.model.jogo import Jogo
+
+class Aquisicao:
+  def __init__ (self, usuario: Usuario, jogo: Jogo, data_aquisicao:str, metodo_pagamento: str):
+    self.__usuario = usuario
+    self.__jogo = jogo
+    self.__data_aquisicao = data_aquisicao
+    self.__metodo_pagamento = metodo_pagamento
+  
+  @property
+  def data_aquisicao(self):
+    return self.__data_aquisicao
+  
+  @data_aquisicao.setter
+  def data_aquisicao(self, data_aquisicao: str):
+    if isinstance(data_aquisicao, str):
+      self.__data_aquisicao = data_aquisicao
+  
+  @property
+  def metodo_pagamento(self):
+    return self.__metodo_pagamento
+  
+  @metodo_pagamento.setter
+  def metodo_pagamento(self, metodo_pagamento: str):
+    if isinstance (metodo_pagamento, str):
+      self.__metodo_pagamento = data_aquisicao
+  
+  @property
+  def usuario(self):
+    return self.__usuario
+  
+  @property
+  def jogo(self):
+    return self.__jogo
--- Model/Usuario.py
+++ Model/Usuario.py
@@ -1,5 +1,6 @@
 class Usuario:
 def __init__(self, nome: str, email: str, id_usuario: str):
+  if isinstance((nome, str) and (email, str)):
     self.__nome = nome
     self.__email = email
     self.__id_usuario = id_usuario
@@ -10,16 +11,18 @@ def __init__(self, nome: str, email: str, id_usuario: str):
     return self.__nome
   
   @nome.setter
-  def nome(self, nome):
-    self.__nome = nome
+  def nome(self, nome: str):
+    if isinstance(nome, str):
+      self.__nome = nome
   
   @property
   def email(self):
     return self.__email
   
   @email.setter
-  def email(self, email):
-    self.__email = email
+  def email(self, email: str):
+    if isinstance(nome, str):
+      self.__email = email
   
   @property
   def aquisicoes(self):
